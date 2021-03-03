ffc679aa441657c35e11e099efff2442f10a65a6
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-03 10:37:59 +0100

    updated skills, deleted link on portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,16 +8,13 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
           <div className="item-wrap">
-            <a href={projects.url} target="_blank" title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
-              <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
+            <img alt={projects.title} src={projectImage} />
+            <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+              </div>
           </div>
         </div>
       })
